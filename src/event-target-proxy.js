7db31e0f7fa1b794641bfb4ebc65d6b829b15195
kodema5 <kodema5@outlook.com>
2023-01-20 14:18:26 -0800

    update property scope

--- src/event-target-proxy.js
+++ src/event-target-proxy.js
@@ -3,43 +3,43 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/EventTarget
 
 export let EventTargetProxy = class {
-    _fId = 0
-    _lmap = {}
+    #fId = 0
+    #lmap = {}
 
-    _listen(name, fn) {
+    #listen(name, fn) {
         let names = name.split('.')
         let ename = names[0]
-        let fname = names[1] || fn.name || `listener-${(++(this._fId))}`
+        let fname = names[1] || fn.name || `listener-${(++(this.#fId))}`
         let lname = `${ename}.${fname}`
 
-        this._remove(lname)
+        this.#remove(lname)
 
-        let lfn = this._lmap[lname] = (e) => {
+        let lfn = this.#lmap[lname] = (e) => {
             fn(e.detail)
         }
-        this._el.addEventListener(ename, lfn)
+        this.et_.addEventListener(ename, lfn)
         return lname
     }
 
-    _dispatch (name, data) {
-        let ce = new CustomEvent(name, { detail:data })
-        this._el.dispatchEvent(ce)
+    #dispatch (name, data, config={}) {
+        let ce = new CustomEvent(name, { ...config, detail:data })
+        this.et_.dispatchEvent(ce)
     }
 
-    _remove(lname) {
-        let lfn = this._lmap[lname]
+    #remove(lname) {
+        let lfn = this.#lmap[lname]
         if (!lfn) return false
 
-        delete this._lmap[lname]
+        delete this.#lmap[lname]
 
         let names = lname.split('.')
         let ename = names[0]
-        this._el.removeEventListener(ename, lfn)
+        this.et_.removeEventListener(ename, lfn)
         return true
     }
 
     constructor(el) {
-        this._el = el instanceof EventTarget
+        this.et_ = el instanceof EventTarget
             ? el
             : (new EventTarget())
 
@@ -47,34 +47,34 @@ export let EventTargetProxy = class {
             set(me, name, fn) {
                 // subscribe if a function
                 if (typeof(fn)==='function') {
-                    return me._listen.apply(me, [name, fn])
+                    return me.#listen.apply(me, [name, fn])
                 }
                 // publish otherwise
                 else {
-                    me._dispatch.apply(me, [name, fn])
+                    me.#dispatch.apply(me, [name, fn])
                     return fn
                 }
             },
             has(me, name) {
-                return me._lmap[name]
+                return me.#lmap[name]
             },
             get(me, name) {
-                return me._lmap[name]
+                return me.#lmap[name]
             },
             deleteProperty(me, regExp) {
                 let names
                 try {
                     let re = new RegExp(regExp)
-                    names = Object.keys(me._lmap).filter((n) => re.test(n))
+                    names = Object.keys(me.#lmap).filter((n) => re.test(n))
                 } catch(_) {
                     names = [regExp]
                 }
-                names.forEach(n => me._remove(n))
+                names.forEach(n => me.#remove(n))
                 return true
             },
             // for Object.getOwnPropertyNames
             ownKeys(me) {
-                return Object.keys(me._lmap)
+                return Object.keys(me.#lmap)
             },
             // for Object.keys
             getOwnPropertyDescriptor(target, prop) {
